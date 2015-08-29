a2d270b2be9e1e85c20ef5a5e09bff0fd95baf79
vik denic <vik.denic@gmail.com>
2015-08-29 10:14:59 -0500

    Change bg color on click
    Also added app icon

--- app/src/main/java/com/nektarlabs/phunphacts/PhunPhactsActivity.java
+++ app/src/main/java/com/nektarlabs/phunphacts/PhunPhactsActivity.java
@@ -1,5 +1,6 @@
 package com.nektarlabs.phunphacts;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,6 +16,7 @@ import java.util.Random;
 public class PhunPhactsActivity extends ActionBarActivity {
 
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,9 @@ public class PhunPhactsActivity extends ActionBarActivity {
 
         //View variables
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.RelativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -31,6 +36,9 @@ public class PhunPhactsActivity extends ActionBarActivity {
                 String fact = mFactBook.getFact();
                 //update label with that fact
                 factLabel.setText(fact);
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
 
