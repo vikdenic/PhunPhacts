346162595005d27877bf2ff44c3e53c9e65a642e
vik denic <vik.denic@gmail.com>
2015-08-29 07:19:55 -0500

    change label on tap
    added onClickListener for showFactButton

--- app/src/main/java/com/nektarlabs/phunphacts/PhunPhactsActivity.java
+++ app/src/main/java/com/nektarlabs/phunphacts/PhunPhactsActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class PhunPhactsActivity extends ActionBarActivity {
 
@@ -11,6 +15,22 @@ public class PhunPhactsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phun_phacts);
+
+        //View variables
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //The button was clicked, so update the label with a new fact
+                String fact = "Ostriches can run faster than horses";
+                factLabel.setText(fact);
+
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
     }
 
     @Override
